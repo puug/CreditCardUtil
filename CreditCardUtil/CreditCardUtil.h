be64442347ff1c27eba7f500d5e11fa7deb70ae9
Ryan Murfitt <rmurfitt@gmail.com>
2012-05-27 20:17:10 +1000

    * Initial commit

--- CreditCardUtil/CreditCardUtil.h
+++ CreditCardUtil/CreditCardUtil.h
@@ -8,6 +8,36 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    MasterCard,
+    Amex,
+    Visa,
+    Diners,
+    Unknown
+} CreditCardType;
+
 @interface CreditCardUtil : NSObject
+
+
+/**
+ Given a Credit Card number, tries to determine the card type, based on the bank card rules.
+ http://en.wikipedia.org/wiki/Bank_card_number
+ 
+ If unable to parse, returns 'Unknown'
+ 
+ Basic implementation of the algorith at http://cuinl.tripod.com/Tips/o-1.htm
+ 
+    CARD TYPES             PREFIX            WIDTH
+    American Express       34, 37            15
+    Diners Club            300 to 305, 36    14
+    Carte Blanche          38                14
+    Discover               6011              16
+    EnRoute                2014, 2149        15
+    JCB                    3                 16
+    JCB                    2131, 1800        15
+    Master Card            51 to 55          16
+    Visa                   4                 13, 16
+ */
++ (CreditCardType)validCardTypeFromCardNumber:(NSString *)creditCardNumber;
 
 @end
--- CreditCardUtil/CreditCardUtil.m
+++ CreditCardUtil/CreditCardUtil.m
@@ -8,6 +8,71 @@
 
 #import "CreditCardUtil.h"
 
+@interface CreditCardUtil (PrivateMethods)
++ (CreditCardType)determineTypeFromCardNumber:(NSString *)creditCardNumber;
++ (CreditCardType)validateCreditCard:(CreditCardType)creditCardType forCardNumber:(NSString*)creditCardNumber;
+@end
+
 @implementation CreditCardUtil
+
++ (CreditCardType)validCardTypeFromCardNumber:(NSString *)creditCardNumber {
+    CreditCardType unvalidatedType = [self determineTypeFromCardNumber:creditCardNumber];
+    return [self validateCreditCard:unvalidatedType forCardNumber:creditCardNumber];
+}
+
++ (CreditCardType)determineTypeFromCardNumber:(NSString *)creditCardNumber {
+    int oneDigitPrefix = [[creditCardNumber substringToIndex:1] intValue];
+    if (oneDigitPrefix == 4) {
+        return Visa;
+    }
+    
+    int twoDigitPrefix = [[creditCardNumber substringToIndex:2] intValue];
+    switch (twoDigitPrefix) {
+        case 34:
+        case 37:
+            return Amex;
+        case 36:
+            return Diners;
+        case 51:
+        case 52:
+        case 53:
+        case 54:
+        case 55:
+            return MasterCard;
+        default:
+            break;
+    }
+    
+    int threeDigitPrefix = [[creditCardNumber substringToIndex:3] intValue];
+    if (threeDigitPrefix >= 300 && threeDigitPrefix <= 305) {
+        return Diners;
+    }
+    
+    return Unknown;
+}
+
++ (CreditCardType)validateCreditCard:(CreditCardType)creditCardType forCardNumber:(NSString*)creditCardNumber {
+    CreditCardType result = Unknown;
+    int cardNumberLength =  [creditCardNumber length];
+    switch (creditCardType) {
+        case Amex:
+            result = (cardNumberLength == 15) ? Amex : Unknown;
+            break;
+        case Diners:
+            result = (cardNumberLength == 14) ? Diners : Unknown;
+            break;
+        case MasterCard:
+            result = (cardNumberLength == 16) ? MasterCard : Unknown;
+            break;
+        case Visa:
+            result = (cardNumberLength == 13 || cardNumberLength == 16) ? Visa : Unknown;
+            break;
+        default:
+            break;
+    }
+    return result;
+}
+
+
 
 @end
